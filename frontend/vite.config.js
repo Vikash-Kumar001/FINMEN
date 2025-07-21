107b32c107112b9cbf977c592dcfa97f6fed217c
Vikash-Kumar001 <vikashkumarsudhi8527@gmail.com>
2025-07-21 20:30:51 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // ✅ Backend server
+        target: "https://finmen.onrender.com/", // ✅ Backend server
         changeOrigin: true,
         secure: false,
       },

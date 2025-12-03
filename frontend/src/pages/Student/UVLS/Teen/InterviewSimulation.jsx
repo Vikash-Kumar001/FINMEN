56e7050e3cb63e924317f86eb31cfe271480c03b
Vikash-Kumar001 <vikashkumarsudhi8527@gmail.com>
2025-12-03 20:00:43 +0530

    feat: GameId fixed for Interview Simulation

--- frontend/src/pages/Student/UVLS/Teen/InterviewSimulation.jsx
+++ frontend/src/pages/Student/UVLS/Teen/InterviewSimulation.jsx
@@ -8,7 +8,7 @@ import { getUvlsTeenGames } from "../../../../pages/Games/GameCategories/UVLS/te
 const InterviewSimulation = () => {
   const location = useLocation();
   
-  const gameId = "uvls-teen-83";
+  const gameId = "uvls-teen-64";
   const gameData = getGameDataById(gameId);
   
   const coinsPerLevel = gameData?.coins || location.state?.coinsPerLevel || 5;
